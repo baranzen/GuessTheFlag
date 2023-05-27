bfcd0a7c241d0d679816ddf68c98532780d7cf91
Baran <baranzengeralp@gmail.com>
2023-05-28 02:02:02 +0300

    done your score

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var countries = ["Estonia","France","Germany","Ireland","Italy","Nigeria","Poland","Russia","Spain","UK","US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color.mint
+                .ignoresSafeArea()
+            
+            VStack (spacing: 30){
+                VStack{
+                    Text("Tap the flag of")
+                        .font(.title)
+                    Text(countries[correctAnswer])
+                }
+                
+                ForEach(0..<3){ number in
+                    Button{
+                        flagTapped(number)
+                    }label:{
+                        Image(countries[number])
+                            .renderingMode(.original    )
+                    }
+                    
+                }
+            }
         }
-        .padding()
+        .alert(scoreTitle,isPresented: $showingScore){
+            Button("Continue",action: askQuestion)
+        }message: {
+            Text("your score is: \(score)")
+        }
+        
+    }
+    
+    func flagTapped(_ number:Int){
+        if number == correctAnswer{
+            scoreTitle = "Correct"
+            score+=1
+            askQuestion()
+        }else{
+            scoreTitle = "Wrong"
+            showingScore = true
+        }
+        
+    }
+    
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
