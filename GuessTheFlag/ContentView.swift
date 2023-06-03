03ee11ab393b60f8b3bed84d8100c67b009e973f
Baran <baranzengeralp@gmail.com>
2023-06-03 17:31:52 +0300

    done reskin of app

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -16,31 +16,61 @@ struct ContentView: View {
     
     var body: some View {
         ZStack{
-            Color.mint
-                .ignoresSafeArea()
+            RadialGradient(stops:[
+                .init(color: Color("color1"), location: 0.3),
+                .init(color:Color("color3"), location: 0.3)
+            ],center: .top, startRadius: 72, endRadius: 700)
+            .ignoresSafeArea()
             
-            VStack (spacing: 30){
-                VStack{
-                    Text("Tap the flag of")
-                        .font(.title)
-                    Text(countries[correctAnswer])
-                }
+            VStack{
+                Spacer()
+                Text("Guess the flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundColor(Color("color2"))
+                    .shadow(radius: 0.7)
                 
-                ForEach(0..<3){ number in
-                    Button{
-                        flagTapped(number)
-                    }label:{
-                        Image(countries[number])
-                            .renderingMode(.original    )
+                VStack (spacing: 30){
+                    VStack{
+                        Text("Tap the flag of")
+                            .font(.subheadline.bold())
+                            .foregroundStyle(.secondary)
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3){ number in
+                        Button{
+                            flagTapped(number)
+                        }label:{
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                        }
+                        
                     }
                     
                 }
+                
+                .padding(.vertical, 20)
+                .padding(.horizontal, 25)
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                
+                Spacer()
+                Text("Your score is: \(score)")
+                    .foregroundColor(Color("color5"))
+                    .bold()
+                    .font(.title2)
+                    .shadow(radius: 0.7)
             }
+            
+            
         }
         .alert(scoreTitle,isPresented: $showingScore){
             Button("Continue",action: askQuestion)
         }message: {
-            Text("your score is: \(score)")
+            Text("Your score is \(score)")
         }
         
     }
